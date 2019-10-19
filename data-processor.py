697be870004a020afed8bb152f01e1ec29d90db5
Chadha <shatha.degachi@gmail.com>
2019-10-19 19:08:03 +0100

    initial commit

---
+++ data-processor.py
@@ -0,0 +1,10 @@
+# Install TensorFlow
+import tensorflow as tf
+import pandas as pd
+import sqlite3
+
+# .db file from additional files of dataset
+metadata = sqlite3.connect("/resources/subset_track_metadata.db")
+metaDataFrame = pd.read_sql_query("select * from songs", metadata)
+# a single track information from dataset
+song = pd.read_hdf("/resources/TRABBKX128F4285205.h5")
