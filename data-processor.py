e95377091f1974401026474fd0cd285427cd1017
wyb15135 <chadha.degachi.2015@strath.ac.uk>
2019-10-19 22:25:58 +0100

    trying to omport data from set

--- data-processor.py
+++ data-processor.py
@@ -1,13 +1,17 @@
 # Install TensorFlow
-import tensorflow as tf
-import pandas as pd
 import sqlite3
 
+import pandas as pd
+import tensorflow as tf
+
 # .db file from additional files of dataset
-metadata = sqlite3.connect("/resources/subset_track_metadata.db")
+metadata = sqlite3.connect("C:/Users/User/PycharmProjects/mer-ml/resources/subset_track_metadata.db")
 metaDataFrame = pd.read_sql_query("select * from songs", metadata)
 # a single track information from dataset
-song = pd.read_hdf("/resources/TRABBKX128F4285205.h5")
+song = pd.read_hdf("C:/Users/User/PycharmProjects/mer-ml/resources/TRABBKX128F4285205.h5")
 
 # load into tensorflow
 dataset = tf.data.Dataset.from_tensor_slices(metaDataFrame.values)
+
+for feat in dataset.take(5):
+    print('Features: {}'.format(feat))
