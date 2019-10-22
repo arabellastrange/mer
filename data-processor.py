67d2cd959411d7182e41793b9e67b26c3747b83f
wyb15135 <chadha.degachi.2015@strath.ac.uk>
2019-10-22 11:41:21 +0100

    reading databases

--- data-processor.py
+++ data-processor.py
@@ -1,17 +1,25 @@
-# Install TensorFlow
 import sqlite3
-
+import h5py
 import pandas as pd
 import tensorflow as tf
 
 # .db file from additional files of dataset
 metadata = sqlite3.connect("C:/Users/User/PycharmProjects/mer-ml/resources/subset_track_metadata.db")
 metaDataFrame = pd.read_sql_query("select * from songs", metadata)
+
 # a single track information from dataset
-song = pd.read_hdf("C:/Users/User/PycharmProjects/mer-ml/resources/TRABBKX128F4285205.h5")
+# throws a conversion error
+# song = pd.read_hdf("C:/Users/User/PycharmProjects/mer-ml/resources/TRABBKX128F4285205.h5")
+song = h5py.File("C:/Users/User/PycharmProjects/mer-ml/resources/TRABBKX128F4285205.h5", 'r')
 
 # load into tensorflow
-dataset = tf.data.Dataset.from_tensor_slices(metaDataFrame.values)
+id = metaDataFrame.pop('track_id')
+# dataset = tf.data.Dataset.from_tensor_slices(metaDataFrame.values)
+# for feat, targ in dataset.take(5):
+#     print('Features and Target: {}'.format(feat, targ))
 
-for feat in dataset.take(5):
-    print('Features: {}'.format(feat))
+print("Keys of metadata table: " + metaDataFrame.keys())
+
+keys = list(song.keys())
+print("Keys of song dataset: ")
+print(*keys)
