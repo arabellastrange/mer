c9f0b3f3af1470855ad1aa8b39ebdd63669c7ecf
wyb15135 <chadha.degachi.2015@strath.ac.uk>
2019-10-22 12:51:18 +0100

    reading keys and song titles

--- src/data-processor-msd.py
+++ src/data-processor-msd.py
@@ -5,24 +5,27 @@ import tensorflow as tf
 
 # .db file from additional files of dataset
 metadata = sqlite3.connect("C:/Users/User/PycharmProjects/mer-ml/resources/subset_track_metadata.db")
-metaDataFrame = pd.read_sql_query("select * from songs", metadata)
 
 # a single track information from dataset
-
 # TODO throws a conversion error?
 # song = pd.read_hdf("C:/Users/User/PycharmProjects/mer-ml/resources/TRABBKX128F4285205.h5")
 song = h5py.File("C:/Users/User/PycharmProjects/mer-ml/resources/TRABBKX128F4285205.h5", 'r')
 
 # load into tensorflow
+metaDataFrame = pd.read_sql("select * from songs", metadata)
 song_id = metaDataFrame.pop('song_id')
 
-dataset = tf.data.Dataset.from_tensor_slices(metaDataFrame.values, song_id)
+#dataset = tf.data.Dataset.from_tensor_slices(metaDataFrame.values, song_id.values)
 
-for feat, targ in dataset.take(5):
-    print('Features and Target: {}'.format(feat, targ))
+# for feat, targ in dataset.take(5):
+#     print('Features and Target: {}'.format(feat, targ))
 
 print("Keys of metadata table: " + metaDataFrame.keys())
+
+print(metaDataFrame.head(5).get("title"))
 
 keys = list(song.keys())
 print("Keys of song dataset: ")
 print(*keys)
+print("Song metadata keys: " )
+print(song.get("metadata").keys())
