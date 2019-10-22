03cd3869e2db732e7535d93e6b61be28700f9d39
wyb15135 <chadha.degachi.2015@strath.ac.uk>
2019-10-22 12:13:52 +0100

    tidy project

--- src/data-processor-msd.py
+++ src/data-processor-msd.py
@@ -8,15 +8,18 @@ metadata = sqlite3.connect("C:/Users/User/PycharmProjects/mer-ml/resources/subse
 metaDataFrame = pd.read_sql_query("select * from songs", metadata)
 
 # a single track information from dataset
-# throws a conversion error
+
+# TODO throws a conversion error?
 # song = pd.read_hdf("C:/Users/User/PycharmProjects/mer-ml/resources/TRABBKX128F4285205.h5")
 song = h5py.File("C:/Users/User/PycharmProjects/mer-ml/resources/TRABBKX128F4285205.h5", 'r')
 
 # load into tensorflow
-id = metaDataFrame.pop('track_id')
-# dataset = tf.data.Dataset.from_tensor_slices(metaDataFrame.values)
-# for feat, targ in dataset.take(5):
-#     print('Features and Target: {}'.format(feat, targ))
+song_id = metaDataFrame.pop('song_id')
+
+dataset = tf.data.Dataset.from_tensor_slices(metaDataFrame.values, song_id)
+
+for feat, targ in dataset.take(5):
+    print('Features and Target: {}'.format(feat, targ))
 
 print("Keys of metadata table: " + metaDataFrame.keys())
 
