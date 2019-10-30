a0cfa19352463ebb375eb0b15c746607f1bae69d
Balqees <balqeesroses@gmail.com>
2019-10-30 18:08:13 +0000

    json to pandas wip

--- src/data-processor-acousticbrainz.py
+++ src/data-processor-acousticbrainz.py
@@ -2,10 +2,30 @@ import pandas as pd
 import requests
 
 # the song Bohemian Rhapsody by Queen has a MBID of: b1a9c0e9-d987-4042-ae91-78d6a3267d69
-response = requests.get("https://acousticbrainz.org/b1a9c0e9-d987-4042-ae91-78d6a3267d69/high-level")
+highlvl_response = requests.get("https://acousticbrainz.org/b1a9c0e9-d987-4042-ae91-78d6a3267d69/high-level")
+lowlvl_response = requests.get("https://acousticbrainz.org/b1a9c0e9-d987-4042-ae91-78d6a3267d69/low-level")
 
-if response.status_code != 200:
-    # This means something went wrong.
-    print('GET /api/mbid/high-level/ {}'.format(response.status_code))
 
-print(response.json())
+def fetch_song_data(response):
+    if response.status_code != 200:
+        # This means something went wrong.
+        print('GET /api/mbid/high-level/ {}'.format(response.status_code))
+
+    json = response.json()
+    print(json)
+    return json
+
+
+def json_to_dataframe(json):
+    # json to dataframe
+    dataframe = pd.read_json(json)
+    print(dataframe)
+
+
+def main():
+    json_to_dataframe(fetch_song_data(highlvl_response))
+    json_to_dataframe(fetch_song_data(lowlvl_response))
+
+
+if __name__ == '__main__':
+    main()
