ea9ff3256bc665e5e72f7bfa56c417e71d5209d0
Balqees <balqeesroses@gmail.com>
2019-10-31 22:01:00 +0000

    drop and extract mood info from high level data

--- src/data-processor-acousticbrainz.py
+++ src/data-processor-acousticbrainz.py
@@ -6,25 +6,98 @@ import requests
 highlvl_response = requests.get("https://acousticbrainz.org/b1a9c0e9-d987-4042-ae91-78d6a3267d69/high-level")
 lowlvl_response = requests.get("https://acousticbrainz.org/b1a9c0e9-d987-4042-ae91-78d6a3267d69/low-level")
 
+mood_columns = ['highlevel.mood_aggressive.all.aggressive', 'highlevel.mood_aggressive.all.not_aggressive',
+                'highlevel.mood_aggressive.probability', 'highlevel.mood_aggressive.value',
+                'highlevel.mood_aggressive.version.essentia',
+                'highlevel.mood_aggressive.version.essentia_build_sha',
+                'highlevel.mood_aggressive.version.essentia_git_sha', 'highlevel.mood_aggressive.version.extractor',
+                'highlevel.mood_aggressive.version.gaia', 'highlevel.mood_aggressive.version.gaia_git_sha',
+                'highlevel.mood_aggressive.version.models_essentia_git_sha',
+                'highlevel.mood_electronic.all.electronic', 'highlevel.mood_electronic.all.not_electronic',
+                'highlevel.mood_electronic.probability', 'highlevel.mood_electronic.value',
+                'highlevel.mood_electronic.version.essentia',
+                'highlevel.mood_electronic.version.essentia_build_sha',
+                'highlevel.mood_electronic.version.essentia_git_sha', 'highlevel.mood_electronic.version.extractor',
+                'highlevel.mood_electronic.version.gaia', 'highlevel.mood_electronic.version.gaia_git_sha',
+                'highlevel.mood_electronic.version.models_essentia_git_sha', 'highlevel.mood_party.all.party',
+                'highlevel.mood_party.all.not_party',
+                'highlevel.mood_party.probability', 'highlevel.mood_party.value',
+                'highlevel.mood_party.version.essentia', 'highlevel.mood_party.version.essentia_build_sha',
+                'highlevel.mood_party.version.essentia_git_sha', 'highlevel.mood_party.version.extractor',
+                'highlevel.mood_party.version.gaia', 'highlevel.mood_party.version.gaia_git_sha',
+                'highlevel.mood_party.version.models_essentia_git_sha', 'highlevel.mood_acoustic.all.acoustic',
+                'highlevel.mood_acoustic.all.not_acoustic', 'highlevel.mood_acoustic.probability',
+                'highlevel.mood_acoustic.value', 'highlevel.mood_acoustic.version.essentia',
+                'highlevel.mood_acoustic.version.essentia_build_sha',
+                'highlevel.mood_acoustic.version.essentia_git_sha',
+                'highlevel.mood_acoustic.version.extractor', 'highlevel.mood_acoustic.version.gaia',
+                'highlevel.mood_acoustic.version.gaia_git_sha',
+                'highlevel.mood_acoustic.version.models_essentia_git_sha',
+                'highlevel.mood_happy.all.happy', 'highlevel.mood_happy.all.not_happy',
+                'highlevel.mood_happy.probability',
+                'highlevel.mood_happy.value', 'highlevel.mood_happy.version.essentia',
+                'highlevel.mood_happy.version.essentia_build_sha',
+                'highlevel.mood_happy.version.essentia_git_sha', 'highlevel.mood_happy.version.extractor',
+                'highlevel.mood_happy.version.gaia', 'highlevel.mood_happy.version.gaia_git_sha',
+                'highlevel.mood_happy.version.models_essentia_git_sha',
+                'highlevel.mood_sad.all.sad', 'highlevel.mood_sad.all.not_sad',
+                'highlevel.mood_sad.probability',
+                'highlevel.mood_sad.value', 'highlevel.mood_sad.version.essentia',
+                'highlevel.mood_sad.version.essentia_build_sha',
+                'highlevel.mood_sad.version.essentia_git_sha', 'highlevel.mood_sad.version.extractor',
+                'highlevel.mood_sad.version.gaia', 'highlevel.mood_sad.version.gaia_git_sha',
+                'highlevel.mood_sad.version.models_essentia_git_sha',
+                'highlevel.mood_relaxed.all.relaxed', 'highlevel.mood_relaxed.all.not_relaxed',
+                'highlevel.mood_relaxed.probability',
+                'highlevel.mood_relaxed.value', 'highlevel.mood_relaxed.version.essentia',
+                'highlevel.mood_relaxed.version.essentia_build_sha',
+                'highlevel.mood_relaxed.version.essentia_git_sha', 'highlevel.mood_relaxed.version.extractor',
+                'highlevel.mood_relaxed.version.gaia', 'highlevel.mood_relaxed.version.gaia_git_sha',
+                'highlevel.mood_relaxed.version.models_essentia_git_sha',
+                'highlevel.moods_mirex.all.Cluster1', 'highlevel.moods_mirex.all.Cluster2',
+                'highlevel.moods_mirex.all.Cluster3', 'highlevel.moods_mirex.all.Cluster4',
+                'highlevel.moods_mirex.all.Cluster5', 'highlevel.moods_mirex.probability',
+                'highlevel.moods_mirex.value', 'highlevel.moods_mirex.version.essentia',
+                'highlevel.moods_mirex.version.essentia_build_sha',
+                'highlevel.moods_mirex.version.essentia_git_sha', 'highlevel.moods_mirex.version.extractor',
+                'highlevel.moods_mirex.version.gaia', 'highlevel.moods_mirex.version.gaia_git_sha',
+                'highlevel.moods_mirex.version.models_essentia_git_sha']
+
 
 def fetch_song_data(response):
     if response.status_code != 200:
         # This means something went wrong.
-        print('GET /api/mbid/high-level/ {}'.format(response.status_code))
+        print('GET /api/mbid/level/ {}'.format(response.status_code))
 
     return response.json()
 
 
 def json_to_dataframe(json):
-    # json to dataframe
-    print(json)
     dataframe = json_normalize(json)
-    print(dataframe)
+    return dataframe
+
+
+def extract_mood_information(dataframe):
+    mood_dataframe = dataframe[mood_columns]
+    return mood_dataframe
+
+
+def drop_mood_information(dataframe):
+    dataframe = dataframe.drop(columns=mood_columns)
+    return dataframe
 
 
 def main():
-    json_to_dataframe(fetch_song_data(highlvl_response))
-    json_to_dataframe(fetch_song_data(lowlvl_response))
+    highlvl_datafrme = json_to_dataframe(fetch_song_data(highlvl_response))
+    lowlvl_dataframe = json_to_dataframe(fetch_song_data(lowlvl_response))
+
+    mood_dataframe = extract_mood_information(highlvl_datafrme)
+    highlvl_datafrme = drop_mood_information(highlvl_datafrme)
+
+    print("output: ")
+    print(highlvl_datafrme)
+    print(lowlvl_dataframe)
+    print(mood_dataframe)
 
 
 if __name__ == '__main__':
